1ccdad4ce13ec75bbc5d96ef22f33a3fa48c36b6
uikanufo <arsiasaarinen@gmail.com>
2022-11-25 15:23:32 +0200

    asd

---
+++ roomgen.py
@@ -0,0 +1,14 @@
+import random
+
+def heita_noppaa():
+    print("heitetään noppaa...")
+    time.sleep(0.1)
+    # random numero juttu
+    print("nopasta tuli", numero)
+    return numero
+
+class room:
+    def __init__(index, colour):
+        index = self.index
+        colour = self.colour
+        searched = False
