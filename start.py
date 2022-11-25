24e9ef12bd1128892d0895dc36341c58c37ec51c
uikanufo <arsiasaarinen@gmail.com>
2022-11-25 15:21:44 +0200

    Added planm Made some crappy "sketches" of room generation

--- start.py
+++ start.py
@@ -1,2 +1,17 @@
-Features wanted:
-    - 3 levelable stats (with abilities)
+class PC:
+    name = ""
+    strength = 0
+    dexterity = 0
+    intelligence = 0
+    #Add hit locations 1-10, make class
+    food = 0
+    bslot1 = ""
+    bslot2 = ""
+    bslot3 = ""
+    bslot4 = ""
+    bslot5 = ""
+    #Figure out a good way to do time tracking
+    HP = 0 
+    #Add spells later
+    backpack = []
+    
