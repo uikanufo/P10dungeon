1ecbabdb52694cace0c384a2e24c18e875745506
uikanufo <arsiasaarinen@gmail.com>
2022-11-28 21:47:12 +0200

    Cool changes

--- roomgen.py
+++ roomgen.py
@@ -1,14 +1,33 @@
 import random
+import time
 
 def heita_noppaa():
     print("heitetään noppaa...")
     time.sleep(0.1)
     # random numero juttu
+    numero = random.randint(1, 10)
     print("nopasta tuli", numero)
     return numero
 
-class room:
-    def __init__(index, colour):
-        index = self.index
-        colour = self.colour
-        searched = False
+perunavero = []
+
+class Room: 
+    parametres = [# colour, open exits
+    ("red", 3,),
+    ("yellow", 2,),
+    ("green", 1) ]
+    def __init__(self):
+        index = random.randint(0,2)
+        self.colour = self.parametres[index][0]
+        self.exits = self.parametres[index][1]
+        self.searched = False
+    def search(self):
+        if self.searched == False:
+            self.searched = True
+            print("Looooot!")
+        else:
+            print("Ahne peruna :P")
+
+peruna = Room()
+peruna.search()
+peruna.search()
